5c2cf6657eb51ad7fbba948bfb1263a176dfcd8a
Ram Narayan <patroramnarayan1@gmail.com>
2015-12-16 17:35:29 +0530

    First Commit

---
+++ src/gitmergetestpack/Gitmergeclass.java
@@ -0,0 +1,13 @@
+package gitmergetestpack;
+
+public class Gitmergeclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("A single line by Ram...");
+		
+	}
+
+}
